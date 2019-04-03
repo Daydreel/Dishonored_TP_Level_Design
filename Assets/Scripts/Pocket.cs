28e842d34a09c61f46f9bd70175f12a791d9f600
Auriel <aurelien2nice@gmail.com>
2019-04-03 15:05:14 +0200

    Aurelien level

---
+++ Assets/Scripts/Pocket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pocket : MonoBehaviour {
+
+	Player_Health Player_Health;
+	void Start ()
+    {
+		Player_Health = GetComponentInParent<Player_Health>();
+	}
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<InteractableObject>() && other.GetComponent<InteractableObject>().isPickup) //Elixir de vie
+        {
+            InteractableObject incommingObject = other.GetComponent<InteractableObject>();
+            if(incommingObject.d == InteractableObject.Description.Elixir && incommingObject.objectType == 0){
+                float healAmont = incommingObject.GetComponent<HealPotion>().HealAmmont;
+                Player_Health.HealPlayer(healAmont);
+            }
+            Destroy(other.gameObject);
+        }
+    }
+}
